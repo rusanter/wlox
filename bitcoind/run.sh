e59f2dec3bf665528be7c6f6324e0c71cddbca2a
David Rodriguez <david@goldenfrogtech.com>
2016-06-28 02:19:21 -0400

    bitcoind works good

---
+++ bitcoind/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+docker run -h bitcoind1 --name bitcoingfts -p 8332:8332 -p 8333:8333 gf
+ts/bitcoind
+####!/usr/bin###/env## bash#####
+
+
+#bitcoind -datadir=/home/test/.bitcoin/ -testnet=1 -conf=/home/test/.bitcoin/bitcoin.conf &
+
+
+#bash
