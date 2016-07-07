55648ca6413c8a560941b89ff6fc9ea1a9121158
Jim Phillips <jim@goldenfrogtech.com>
2016-07-07 22:50:43 +0000

    FIXED: dirroot was not set

--- cron/docker_cfg.php
+++ cron/docker_cfg.php
@@ -1,6 +1,7 @@
 <?php 
 class object {}
 $CFG = new object();
+$CFG->dirroot = '/app/';
 $CFG->dbhost = $_ENV["dbhost"] ?: 'db';
 $CFG->dbname = $_ENV["dbname"] ?: 'wlox';
 $CFG->dbuser = $_ENV["dbuser"] ?: 'wlox';
