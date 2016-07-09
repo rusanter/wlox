19f14b72b45b3ee27a742ca98ebdad546cdaf7ad
Jim Phillips <jim@goldenfrogtech.com>
2016-07-09 00:18:42 +0000

    FIXED: database doesn't init properly

--- db/docker_db.sh
+++ db/docker_db.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 if [ ! -z "${WLOX_URL}" ]; then
-	echo "UPDATE app_configuration SET frontend_baseurl = \'${WLOX_URL}\';" >> /docker-entrypoint-initdb.d/02_docker.sql
+	echo "UPDATE app_configuration SET frontend_baseurl = '${WLOX_URL}';" >> /tmp/envvars.sql
 else
-	echo "UPDATE app_configuration SET frontend_baseurl = \'/\';" >> /docker-entrypoint-initdb.d/02_docker.sql
+	echo "UPDATE app_configuration SET frontend_baseurl = '/';" >> /tmp/envvars.sql
 fi
+
+/usr/bin/mysql -u ${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DATABASE} < /tmp/envvars.sql 
