090c2c377fa0aed8a65a8d969cbfe968f0f96910
Mirell <lucerstar21@gmail.com>
2016-07-12 12:33:17 -0400

    Add files via upload

--- frontend/htdocs/includes/head.php
+++ frontend/htdocs/includes/head.php
@@ -31,7 +31,7 @@
     
     <!-- ######### CSS STYLES ######### -->
 	<!--<link rel="stylesheet" href="css/style.css?v=20150625" type="text/css" />-->
-    <link rel="stylesheet" href="css/style.css?v=20150668" type="text/css" />  
+    <link rel="stylesheet" href="css/style.css?v=20150669" type="text/css" />  
     <link rel="stylesheet" href="css/font-awesome/css/font-awesome.min.css">
     
     <!-- responsive devices styles -->
@@ -139,12 +139,12 @@
                 </li>
                
                 <li>
-                    <div class="navbar-brand" href="register.php">
+                    <div class="navbar-brand" style="margin-left: -7px" href="register.php">
                       <a href="register.php">  
                         <img src="images/elements/icon-signup.svg" alt="icon wallet" height="33" width="33">
                         </a>
                      </div>
-                    <div class="navbar-brand marg" style="margin-left: -7px" href="register.php">
+                    <div class="navbar-brand marg"  href="register.php">
                        <a href="register.php"> 
                          <span> <?= Lang::string('home-register') ?></span>
                         </a>
